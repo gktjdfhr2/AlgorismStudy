921eb930bea313a98eb2b760d2608d19ed8638b5
DESKTOP-L2F7LSG\COM <gktjdfhr2@naver.com>
2022-03-18 09:32:19 +0900

    dailyAlgorism 03/17 refactoring

--- src/dailyAlgorism/HSR_2217.java
+++ src/dailyAlgorism/HSR_2217.java
@@ -3,19 +3,24 @@ package dailyAlgorism;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+
 
 public class HSR_2217 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] rope = new int[N];
-		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int[] rope = new int[N];		
+				
 		for (int i = 0; i < N; i++) {
-			rope[i]= Integer.parseInt(br.readLine());
-			if (rope[i] < min) {
-				min = rope[i];
-			}
+			rope[i] = Integer.parseInt(br.readLine());			
 		}
-		System.out.println(min * N);	
+		Arrays.sort(rope);
+		
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, rope[i] * (N - i));
+		}
+		System.out.println(max);	
 	}
 }
