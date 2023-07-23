3f0e0ab914208b6dc022921360d86f1505846286
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-07-23 23:08:28 +0900

    2023-07-23

--- src/programmers/answer/Answer_169198.java
+++ src/programmers/answer/Answer_169198.java
@@ -1,16 +1,5 @@
 package programmers.answer;
 
-<<<<<<< HEAD
-public class Answer_169198 {
-    public static void main(String [] args) {
-         solution(10, 10, 3, 7 , new int[][]{{7, 7}, {2, 7}, {7, 3}});
-    }
-
-    public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
-        int[] answer = {};
-        return answer;
-    }
-=======
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,5 +29,5 @@ public class Answer_169198 {
 
 
     }
->>>>>>> origin/master
+
 }
