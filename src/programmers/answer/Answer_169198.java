86e1f875a9204104066f91b54e99565ce05f1b6f
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-07-23 22:37:22 +0900

    2023-07-23

---
+++ src/programmers/answer/Answer_169198.java
@@ -0,0 +1,12 @@
+package programmers.answer;
+
+public class Answer_169198 {
+    public static void main(String [] args) {
+         solution(10, 10, 3, 7 , new int[][]{{7, 7}, {2, 7}, {7, 3}});
+    }
+
+    public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/programmers/answer/TestClass.java
@@ -0,0 +1,62 @@
+package programmers.answer;
+
+import javax.swing.text.html.Option;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class TestClass {
+    public static void main(String [] args) {
+        String[] name = new String [] {"may", "kein", "kain", "radi"};
+        int[] yearning = new int[] {5, 10, 1, 3};
+        String[][] photo = new String[][] {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+
+        Map<String, Integer> map = new HashMap<>();
+        Map<Integer, String> map2 = new TreeMap<>();
+        
+        for(int i = 0; i < name.length; i++) {
+            System.out.println( i + " " + name[i] + " " + yearning[i]);
+            map.put(name[i] , yearning[i]);
+        }
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map3 = new HashMap<>();
+
+
+        BigDecimal num = BigDecimal.valueOf(150000);
+        int[] test = new int[4];
+
+
+        test = System.arr
+        System.out.println(photo[1][3]);
+        int size = photo.length;
+        int[] result = new int[size];
+        for(int i = 0; i< photo.length; i++) {
+            for(int j = 0; j < photo[i].length; j++) {
+                System.out.println("photo["+ i + "]" + "[" + j +  "] = " + photo[i][j] );
+                if(map.containsKey(photo[i][j])){
+                    result[i] += map.get(photo[i][j]);
+                }
+            }
+        }
+
+        for (int num:result
+             ) {
+            System.out.println(num);
+        }
+
+    }
+
+    public static int findIndex(String[] arr, String name) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].equals(name)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void swap(String[] arr, int index) {
+        String temp = arr[index];
+        arr[index] = arr[index -1];
+        arr[index -1] = temp;
+    }
+}
